1ad585c534518158fb3511af592f1a12ce3e2b23
joaopjk <joaopjk48@gmail.com>
2025-08-07 14:37:14 -0300

    Singleton Thread Safety

---
+++ CSharpEstudos/SingletonThreadSafety/Program.cs
@@ -0,0 +1,18 @@
+﻿using SingletonThreadSafety;
+
+Parallel.Invoke(
+    AcessoThread1,
+                 AcessoThread2
+    );
+
+static void AcessoThread1()
+{
+    var s1 = Singleton.Instance;
+    Console.WriteLine("Thread 1");
+}
+
+static void AcessoThread2()
+{
+    var s2 = Singleton.Instance;
+    Console.WriteLine("Thread 2");
+}
---
+++ CSharpEstudos/SingletonThreadSafety/Singleton.cs
@@ -0,0 +1,27 @@
+﻿namespace SingletonThreadSafety
+{
+    internal class Singleton
+    {
+        private static Singleton _instance;
+        private static int _contador = 0;
+        private static readonly Lock Lock = new Lock();
+
+        private Singleton()
+        {
+            _contador++;
+            Console.WriteLine($"Instância: {_contador}");
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+                lock (Lock)
+                {
+                    return _instance ??= new Singleton();
+                }
+            }
+        }
+    }
+}

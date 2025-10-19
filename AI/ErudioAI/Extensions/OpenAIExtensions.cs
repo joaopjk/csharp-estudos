254e36753e3bdc1e311ae5c7f0d7d865fd7c7b3e
joaop <joaopjk48@gmail.com>
2025-10-19 18:43:33 -0300

    Configurando conexão com o open api

---
+++ AI/ErudioAI/Extensions/OpenAIExtensions.cs
@@ -0,0 +1,16 @@
+﻿using OpenAI;
+
+namespace ErudioAI.Extensions;
+
+public static class OpenAiExtensions
+{
+    public static WebApplicationBuilder AddOpenAiExtensions(this WebApplicationBuilder builder)
+    {
+        var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY")!;
+        var openAiClient = new OpenAIClient(apiKey);
+        
+        builder.Services.AddSingleton(openAiClient);
+
+        return builder;
+    }
+}

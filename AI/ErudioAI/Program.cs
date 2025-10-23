e279fbb7ec67c473cbb4cc6b011502e3b7657554
joaop <joaopjk48@gmail.com>
2025-10-22 21:58:23 -0300

    Edit cors policy

--- AI/ErudioAI/Program.cs
+++ AI/ErudioAI/Program.cs
@@ -3,16 +3,23 @@ using ErudioAI.Services;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.AddOpenAiExtensions();
 
+builder.AddOpenAiExtensions();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSingleton<ChatService>();
 builder.Services.AddSingleton<RecipeService>();
 builder.Services.AddSingleton<ImageService>();
+builder.Services.AddCors(opt => opt.AddDefaultPolicy(
+    corsPolicyBuilder => corsPolicyBuilder
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+    ));
 
 var app = builder.Build();
 
+app.UseCors();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
